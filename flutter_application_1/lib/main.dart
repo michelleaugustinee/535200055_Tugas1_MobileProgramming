d44b5f900d4041b271fede4289a55b586d5484f5
Michelle Augustine <michelleaugustinee@gmail.com>
2022-02-16 14:37:20 +0700

    #1 Hello World

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+          backgroundColor: Colors.teal,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Image(
+                image: NetworkImage(
+                    'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
+              ),
+              Text('Hello World'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
